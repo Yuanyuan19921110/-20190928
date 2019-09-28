9e9a454cf68d647fa6e5c4606625948d50d94223
Yuanyuan19921110 <381769019@qq.com>
2019-09-28 11:02:42 +0800

    第一次push

---
+++ src/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;//前5行是什么意思
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static int solveMeFirst(int a, int b) {//?
+      	// Hint: Type return a+b; below 
+    	return (a+b);//return函数的意义？
+   }
+
+ public static void main(String[] args) {//主函数不应该是第一位嘛？
+        Scanner in = new Scanner(System.in);//含义？
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
---
+++ src/Solution2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Solution2 {
+
+ public static void main(String[] args) {
+      
+      Scanner scan= new Scanner(System.in);
+        
+        int a= scan.nextInt();
+        int b= scan.nextInt();
+        add(a,b);
+        }   
+ public static void add(int a ,int b) {
+	 System.out.println(a+b);
+ }
+
+   }
+
+ 
+
