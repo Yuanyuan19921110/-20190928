3853040596dc757a6af2b837169c51fadf505a85
Yuanyuan19921110 <381769019@qq.com>
2019-11-03 19:42:15 +0800

    .java编译成.class文件（流程图）

---
+++ src/lesson03/Practice07.java
@@ -0,0 +1,48 @@
+package lesson03;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Practice07 {
+
+	
+		  // Complete the primality function below.
+	static String primality(int n) {
+	
+	    	if(n==1)return"Not prime";
+	        else if(n==2)return"prime" ;
+	    	
+	        for(int i=2;i<=n;i++) 
+	        if(n%i==0) return "Not prime";
+	        return "prime";
+	    
+	  
+	    }
+	    
+
+	    private static final Scanner scanner = new Scanner(System.in);
+
+	    public static void main(String[] args) throws IOException {
+	        
+
+	        int p = scanner.nextInt();
+	        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+	        for (int pItr = 0; pItr < p; pItr++) {
+	            int n = scanner.nextInt();
+	            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+	            String result = primality(n);
+	            
+	            System.out.println(result);
+	            
+	        }
+
+	   
+
+	        scanner.close();
+	    
+
+	}
+
+}
