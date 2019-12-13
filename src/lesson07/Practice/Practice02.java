42f417026e8177d5959977864cd35f11e39855dd
Yuanyuan19921110 <381769019@qq.com>
2019-12-13 22:21:40 +0800

    课后题7

--- src/lesson07/Practice/Practice02.java
+++ src/lesson07/Practice/Practice02.java
@@ -1,5 +1,52 @@
 package lesson07.Practice;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Practice02 {
 
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+       Integer[] arr=new Integer[s.size()];
+       s.toArray(arr);
+       for(int i=0;i<arr.length;i++) { 
+    	   if()
+       }
+       
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
+
--- src/lesson07/Practice/Practice03.java
+++ src/lesson07/Practice/Practice03.java
@@ -10,8 +10,21 @@ import java.util.regex.*;
 public class Practice03 {
 
     // Complete the divisibleSumPairs function below.
-    static int divisibleSumPairs(int n, int k, int[] ar) {
-
+    static int divisibleSumPairs(int n, int k, int[] ar) { 
+     
+    		int sum=0;
+    		for(int i=0;i<n;i++) {
+    		for(int j=0;j<n;j++) {
+    		
+    	if(i<j &&(ar[i]+ar[j])%k==0) {
+    		sum++;
+    	}
+    	}
+    	}
+    	
+    	return sum;
+    	
+    
 
     }
 
